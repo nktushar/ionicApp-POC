cef415694e0afdf722ef209d2dceed4e35945aa1
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2024-01-04 16:01:40 +0600

    trying toast ui

--- src/app/pages/toast-calendar/toast-calendar.page.ts
+++ src/app/pages/toast-calendar/toast-calendar.page.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+// import Calendar from '@toast-ui/calendar';
+
 
 @Component({
   selector: 'app-toast-calendar',
   templateUrl: './toast-calendar.page.html',
   styleUrls: ['./toast-calendar.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class ToastCalendarPage implements OnInit {
+
+  
 
   constructor() { }
 
