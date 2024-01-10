2ec7b42624297359fc614661a1dc73e05c98e5f4
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2024-01-10 23:39:44 +0600

    dkllj

--- src/app/pages/design-component/design-component.page.ts
+++ src/app/pages/design-component/design-component.page.ts
@@ -8,16 +8,16 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './design-component.page.html',
   styleUrls: ['./design-component.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class DesignComponentPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   // for event details box
   showLess = true;
 
+  longText =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla.';
 }
