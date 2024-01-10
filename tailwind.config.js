50fa2eb94d0cf9b2271e18481430dc59559922f2
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2024-01-10 14:07:02 +0600

    font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    fontFamily: {
+      // sans: ['"Segoe UI"', "Roboto", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
   },
   plugins: [],
 };
